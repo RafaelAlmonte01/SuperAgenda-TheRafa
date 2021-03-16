4d77c6959629ad5fb11409a2a7666e1da3dd95a0
RafaelAlmonte01 <62627526+RafaelAlmonte01@users.noreply.github.com>
2021-03-16 09:19:15 -0400

    Agregar archivos de proyecto.

---
+++ SuperAgenda-TheRafa/Models/Contactos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperAgenda_TheRafa.Models
+{
+    [Table("Contactos")]
+    public class Contactos
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Nombres { get; set; }
+        [Required]
+        public string Apellidos { get; set; }
+        [Required]
+        public string TelefonoMovil { get; set; }
+        [MaxLength(14)]
+        public string TelefonoCasa { get; set; }
+        [MaxLength(14)]
+        public string TelefonoTrabajo { get; set; }
+        public string Empresa { get; set; }
+        public string Direccion { get; set; }
+        public string Cedula { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Instagram { get; set; }
+        public string Facebook { get; set; }
+        public string PaginaWeb { get; set; }
+        public string PuestoTrabajo { get; set; }
+        public Byte[] Foto { get; set; }
+
+
+    }
+}
---
+++ SuperAgenda-TheRafa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuperAgenda_TheRafa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
